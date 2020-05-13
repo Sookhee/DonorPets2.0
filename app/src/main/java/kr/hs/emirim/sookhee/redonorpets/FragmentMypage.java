c0d549f9ff4165421979b36e5c1a557cb0a61e0e
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-14 00:26:09 +0900

    mypageFragment 관심있는 보호소 adapter 연결

--- app/src/main/java/kr/hs/emirim/sookhee/redonorpets/FragmentMypage.java
+++ app/src/main/java/kr/hs/emirim/sookhee/redonorpets/FragmentMypage.java
@@ -8,9 +8,85 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import kr.hs.emirim.sookhee.redonorpets.adapter.ShelterAdapter;
+import kr.hs.emirim.sookhee.redonorpets.adapter.StoryAdapter;
+import kr.hs.emirim.sookhee.redonorpets.model.ShelterData;
+import kr.hs.emirim.sookhee.redonorpets.model.StoryData;
 
 public class FragmentMypage extends Fragment {
+    View mypageView;
+    RecyclerView recyclerView;
+    LinearLayoutManager mLayoutManager;
+    ShelterAdapter adapter;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mRef = database.getReference().child("shelter");
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.mypageView = view;
+
+        recyclerView = (RecyclerView)mypageView.findViewById(R.id.mypageShelterRecyclerView);
+
+        resetShelterRecyclerView(mypageView);
+    }
+
+    public void resetShelterRecyclerView(View view){
+
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String key = dataSnapshot.getKey();
+                ShelterData shelterData = dataSnapshot.getValue(ShelterData.class);
+
+                adapter.addDataAndUpdate(key, shelterData);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String key = dataSnapshot.getKey();
+                ShelterData shelterData = dataSnapshot.getValue(ShelterData.class);
+
+                adapter.addDataAndUpdate(key, shelterData);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                adapter.deleteDataAndUpdate(key);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        adapter = new ShelterAdapter(getActivity());
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
